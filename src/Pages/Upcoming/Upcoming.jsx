5c30f285e5bf0211965fe08a2e28063a99256eb1
SonjoyDaGhost <sonjoy.soren.1112@gmaail.com>
2025-01-27 03:43:57 +0600

    added upcomoning mealCard

--- src/Pages/Upcoming/Upcoming.jsx
+++ src/Pages/Upcoming/Upcoming.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import useUpcomingMeals from '../../hooks/useUpcomingMeals';
 
 const Upcoming = () => {
+    const [upcomingMeals] = useUpcomingMeals();
     return (
         <div>
-            <h2 className='text-3xl text-center'>Upcoming Meals:</h2>
+            <h2 className='text-3xl text-center'>Upcoming Meals: {upcomingMeals}</h2>
+            <div>
+                
+            </div>
         </div>
     );
 };
