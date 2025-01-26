87054ff6e2bc2c3a3a010cae59ea5b4a64349d2a
SonjoyDaGhost <sonjoy.soren.1112@gmaail.com>
2025-01-27 03:12:02 +0600

    fillter functionality is added to all meals

--- src/Pages/Upcoming/Upcoming.jsx
+++ src/Pages/Upcoming/Upcoming.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Upcoming = () => {
     return (
         <div>
-            <h2>Upcoming Meals</h2>
+            <h2>Upcoming Meals coming</h2>
         </div>
     );
 };
